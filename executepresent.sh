fc1b39b80afc0dadb1fdc912d840020c4de1e379
derpferpmerp <47375701+derpferpmerp@users.noreply.github.com>
2021-01-22 11:42:16 -0600

    Add files via upload

---
+++ executepresent.sh
@@ -0,0 +1,14 @@
+mkdir encryptedlink
+cd encryptedlink
+curl https://raw.githubusercontent.com/derpferpmerp/ROT13EncryptedLink/master/encryptedrot13.txt -o encrypted.txt
+chmod 777 encrypted.txt
+cat encrypted.txt
+curl https://raw.githubusercontent.com/derpferpmerp/ROT13EncryptedLink/master/main.py -o decrypter.py
+chmod 777 decrypter.py
+echo " Working.... "
+echo " \n\nExecuting decrypter.py"
+python decrypter.py
+echo " ! = :"
+echo " _ = /"
+echo " * = ."
+echo " Open the Link (Once you\'ve changed those characters) in the browser "
